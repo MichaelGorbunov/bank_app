db7cd75071ee80b28d245b9c151cf9793c8e735b
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-21 20:42:42 +0300

    add help string

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
 def filter_dict_list(in_dict_list: list, in_state: str = "EXECUTED") -> list:
+    '''функция принимает список словарей с деталями опреаций и возвращает
+отсортированные по одному из состояний[ "EXECUTED", "CANCELED"].
+Сортировкой по умолчанию является "EXECUTED" '''
     if in_state not in ["EXECUTED", "CANCELED"]:
         return ["error"]
     sort_list = []
