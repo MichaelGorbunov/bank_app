e25ae3b611070368d6adc267edf8c607901ea0d9
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-24 08:02:12 +0300

    изменение названий опреаторов

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,22 @@
-def filter_operations(in_dict_list: list[dict], in_state: str = "EXECUTED") -> list[dict] | None:
+def filter_operations(operation: list[dict], in_state: str = "EXECUTED") -> list[dict] | None:
     """Функция принимает список словарей с деталями операций и возвращает
     отсортированные по одному из состояний ["EXECUTED", "CANCELED"].
     Сортировкой по умолчанию является "EXECUTED" """
     if in_state not in ["EXECUTED", "CANCELED"]:
         return None
     sort_list = []
-    for item in in_dict_list:
+    for item in operation:
         if item.get("state") == in_state:
             # print(item)
             sort_list.append(item)
     return sort_list
 
 
-def sorted_operation(in_sort_list: list[dict], reverse_direction: bool = True) -> list[dict] :
+def sorted_operation(operation: list[dict], reverse_direction: bool = True) -> list[dict] :
     """Функцию принимает на вход список словарей и возвращает новый список,
     в котором исходные словари отсортированы по убыванию даты (ключ date).
     Функция принимает два аргумента, второй необязательный задает
     порядок сортировки (убывание, возрастание)."""
-    sorted_list = sorted(in_sort_list, key=lambda item: item["date"], reverse=reverse_direction)
+    sorted_list = sorted(operation, key=lambda item: item["date"], reverse=reverse_direction)
     return sorted_list
 
