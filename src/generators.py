2aa7fa0b09f86e4663e86345d2b38efb0cdd20c0
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-28 13:57:40 +0300

    модуль generators, функция transaction_descriptions,filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -36,7 +36,85 @@ for card_number in card_number_generator(1, 5):
 0000 0000 0000 0003
 0000 0000 0000 0004
 0000 0000 0000 0005"""
-
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
 
 def card_number_generator(start:int, stop:int)-> str |None:
     """генератор номеров банковских карт в формате
@@ -54,5 +132,45 @@ XXXX XXXX XXXX XXXX в диапазоне от start до stop"""
     return card_number_list
 
 
-for card_number in card_number_generator(9999999999999998, 9999999999999999):
-    print(card_number)
+# for card_number in card_number_generator(9999999999999998, 9999999999999999):
+#     print(card_number)
+# тест card_number in card_number_generator
+def filter_by_currency(transactions:list[dict],currency:str = "USD")->int:
+    filtered_transac = (filter(lambda x: x["operationAmount"]["currency"]["name"] == "USD", transactions))
+    return filtered_transac
+
+
+# filtered_people = list(filter(lambda x: x["operationAmount"]["currency"]["name"] == "USD", transactions))
+# print(filtered_people)  # Output: [{'name': 'Bob', 'age': 30}, {'name': 'David', 'age': 30}]
+# for item in transactions[0]:
+#     print(item)
+usd_transactions = filter_by_currency(transactions, "USD")
+
+for _ in range(2):
+    print(next(usd_transactions)["id"])
+
+# def transaction_descriptions(transactions:list[dict]):
+#     n=[]
+#     for operation in transactions:
+#         n.append(operation["description"])
+#     return n
+
+
+
+#
+#
+#
+
+
+def transaction_descriptions(transactions:list[dict]):
+    for item in transactions:
+        yield item["description"]
+descriptions = transaction_descriptions(transactions)
+for _ in range(5):
+    print(next(descriptions))
+# transaction_descriptions(transactions)
+
+# print(next(transaction_descriptions(transactions)))
+# print(next(transaction_descriptions(transactions)))
+# print(next(transaction_descriptions(transactions)))
+# print(next(transaction_descriptions(transactions)))
