19426f93020c8d942a60c0f499d3825c35b95e1c
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-28 12:44:28 +0300

    модуль generators, функция card_number_generator

---
+++ src/generators.py
@@ -0,0 +1,58 @@
+"""Реализуйте функцию, которая принимает список словарей с банковскими операциями (или объект-генератор, который выдает по одной банковской операции) и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта.
+Пример вызова функции:
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(2):
+    print(next(usd_transactions)["id"])
+Пример вывода:
+
+939719570
+142264268
+Напишите генератор, который принимает список словарей и возвращает описание каждой операции по очереди.
+Пример вызова функции:
+descriptions = transaction_descriptions(transactions):
+for _ in range(5):
+    print(next(descriptions)
+
+Пример вывода:
+
+Перевод организации
+Перевод со счета на счет
+Перевод со счета на счет
+Перевод с карты на карту
+Перевод организации
+
+Напишите генератор номеров банковских карт, который должен генерировать номера карт в формате
+XXXX XXXX XXXX XXXX
+, где
+X
+— цифра. Должны быть сгенерированы номера карт в заданном диапазоне, например от 0000 0000 0000 0001 до 9999 9999 9999 9999 (диапазоны передаются как параметры генератора).
+Пример вызова функции:
+
+for card_number in card_number_generator(1, 5):
+    print(card_number)
+
+0000 0000 0000 0001
+0000 0000 0000 0002
+0000 0000 0000 0003
+0000 0000 0000 0004
+0000 0000 0000 0005"""
+
+
+def card_number_generator(start:int, stop:int)-> str |None:
+    """генератор номеров банковских карт в формате
+XXXX XXXX XXXX XXXX в диапазоне от start до stop"""
+
+    if stop < start or stop > 10000000000000000:
+        return None
+    card_number_list:list[str] = []
+    for i in range(start, stop + 1):
+        nm = list(str(i).zfill(16))
+        nm.insert(4, " ")
+        nm.insert(9, " ")
+        nm.insert(14, " ")
+        card_number_list.append("".join(nm))
+    return card_number_list
+
+
+for card_number in card_number_generator(9999999999999998, 9999999999999999):
+    print(card_number)
