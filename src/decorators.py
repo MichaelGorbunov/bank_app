cbb27e6d0f89159070e8dfd5251c3c85435f0872
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-31 12:23:40 +0300

    print в файл

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,8 @@
 import os
+
+# from functools import wraps
+
+
 # def log(filename: str | None = None) -> None:
 #     if filename is None:
 #         print("hi")
@@ -14,29 +18,43 @@ import os
 #             print("Файл существует")
 #         else:
 #             print("Файл не существует")
+def mprint(text, cons: bool = True):
+    if cons is True:
+        print(text)
+    else:
+        with open("myfile.txt", "w", encoding="UTF-8") as file:
+            print(text, file=file)  # вызываем функцию print, вывод которой должен записаться в файл
+            file.close()
 
 
 def decorator_with_args(filename: str | None = None) -> None:
+
     def my_decorator(func):
+        # @wraps(func)
         def wrapper(*args, **kwargs):
-            print(f"Аргументы декоратора: {filename}")
+            # print(f"Аргументы декоратора: {filename}")
             try:
                 result = func(*args, **kwargs)
+                mprint(f"{func.__name__} ok", False)
                 return result
-            except Exception as e: print(e)
 
-            print("После выполнения функции")
+            except Exception as e:
+                mprint(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}", False)
+
+            # print("После выполнения функции")
 
         return wrapper
+
     return my_decorator
 
 
 @decorator_with_args(filename="myfile.txt")
-def my_function2(x, y):
-    print(f"Выполнение функции с аргументами {x} и {y}")
-    print(x/y)
+def my_function1(x, y):
+    # print(f"Выполнение функции с аргументами {x} и {y}")
+    return x / y
 
-my_function2(10, 2)
+
+print(my_function1(10, 0))
 
 # @log(filename="myfile.txt")
 # def my_function(x, y):
@@ -49,3 +67,6 @@ my_function2(10, 2)
 # except:
 #     print("Деление на 0")
 #     print("Исключение было обработано")
+
+
+# mprint(f"qweq{1+1}eqweq",False)#
