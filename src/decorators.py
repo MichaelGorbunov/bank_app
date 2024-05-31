aa226990ad868fd95ab7ae6fb6a549d25b0c71b4
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-31 11:22:46 +0300

    try except

--- src/decorators.py
+++ src/decorators.py
@@ -20,9 +20,13 @@ def decorator_with_args(filename: str | None = None) -> None:
     def my_decorator(func):
         def wrapper(*args, **kwargs):
             print(f"Аргументы декоратора: {filename}")
-            result = func(*args, **kwargs)
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e: print(e)
+
             print("После выполнения функции")
-            return result
+
         return wrapper
     return my_decorator
 
@@ -30,11 +34,18 @@ def decorator_with_args(filename: str | None = None) -> None:
 @decorator_with_args(filename="myfile.txt")
 def my_function2(x, y):
     print(f"Выполнение функции с аргументами {x} и {y}")
+    print(x/y)
 
-my_function2(10, 20)
+my_function2(10, 2)
 
 # @log(filename="myfile.txt")
 # def my_function(x, y):
 #     return x + y
 #
 # print(my_function(1, 2))
+# try:
+#     for i in range(3):
+#         print(3/i)
+# except:
+#     print("Деление на 0")
+#     print("Исключение было обработано")
